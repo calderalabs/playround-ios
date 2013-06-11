0f4456e507e8c1d01d656d203447b42176ba2372
Eugenio Depalo <eugeniodepalo@gmail.com>
2013-06-11 14:37:12 +0200

    Started buddies controller.

---
+++ Playround/PRBuddiesViewController.h
@@ -0,0 +1,11 @@
+//
+//  PRBuddiesViewController.h
+//  Playround
+//
+//  Created by Eugenio Depalo on 6/11/13.
+//  Copyright (c) 2013 Eugenio Depalo. All rights reserved.
+//
+
+@interface PRBuddiesViewController : UITableViewController
+
+@end
---
+++ Playround/PRBuddiesViewController.m
@@ -0,0 +1,32 @@
+//
+//  PRBuddiesViewController.m
+//  Playround
+//
+//  Created by Eugenio Depalo on 6/11/13.
+//  Copyright (c) 2013 Eugenio Depalo. All rights reserved.
+//
+
+#import "PRBuddiesViewController.h"
+
+@interface PRBuddiesViewController ()
+
+@end
+
+@implementation PRBuddiesViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    
+    if(self) {
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
